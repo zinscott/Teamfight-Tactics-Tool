0ce67c3269398f1e86f14a2f978a32ea6ae99f9b
Scott <scottzin7@gmail.com>
2026-08-11 11:24:58 -0700

    added new rules to supabase

---
+++ supabase/migrations/20260811175003_add_match_history_queue_id.sql
@@ -0,0 +1,1 @@
+ALTER TABLE match_history ADD COLUMN queue_id int;
---
+++ supabase/migrations/20260811175258_nullable_tier_division.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tracked_summoners ALTER COLUMN tier DROP NOT NULL;
+ALTER TABLE tracked_summoners ALTER COLUMN division DROP NOT NULL;
