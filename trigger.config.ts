81dce3d3592248101acf31621f1db822807276c7
Scott <scottzin7@gmail.com>
2026-08-11 13:34:44 -0700

    updated runtime to node-22

--- trigger.config.ts
+++ trigger.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
   project: "proj_lnspwnlthdzyqmyfqdmb",
-  runtime: "node",
+  runtime: "node-22",
   logLevel: "log",
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
   // You can override this on an individual task.
